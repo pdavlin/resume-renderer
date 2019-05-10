884c03b523b2a8431469275daf6980bd2a3af6e0
Price Comstock <price.comstock@gmail.com>
2019-05-10 17:50:31 -0500

    changed wording on powerrank.io description

--- resumeData.js
+++ resumeData.js
@@ -101,15 +101,14 @@ const data = {
       name: "Powerrank.io",
       highlights: [
         "Fully responsive web app that calculates aggregate “power rankings” based on ranked-choice poll results.",
-        "Vue.js single-page application frontend with Vuex state management and Webpack bundling.",
-        "Node.js REST API backend using MongoDB via Mongoose."
+        "Vue.js SPA frontend bundled with Webpack. State managed with Vuex. Node.js REST API backend using MongoDB via Mongoose."
       ],
       link: "https://powerrank.pc.codes"
     },
     {
       name: "Inspopaper",
       highlights: [
-        "App available on Slack app directory that creates novelty desktop wallpapers on demand from messages posted in Slack.",
+        "Slack app available on store that creates novelty desktop wallpapers on demand from messages posted in Slack.",
         "Serverless architecture using Python Google Cloud Functions and Google Cloud Blob Storage."
       ],
       link: "https://inspopaper.fun"
@@ -124,7 +123,7 @@ const data = {
     {
       name: "Powerpoint Generator",
       highlights: [
-        "Procedurally genrates PowerPoint presentations for fun or in emergency situations."
+        "Procedurally generates PowerPoint presentations for fun or for important meetings."
       ],
       link: "https://forgotmyslides.xyz"
     }
