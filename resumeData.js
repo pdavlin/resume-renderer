1be86ef9692ee210e773fc5dfad76be66ccbf9a9
Price Comstock <price.comstock@gmail.com>
2019-05-10 17:32:28 -0500

    added linkified projects

--- resumeData.js
+++ resumeData.js
@@ -103,26 +103,30 @@ const data = {
         "Fully responsive web app that calculates aggregate “power rankings” based on ranked-choice poll results.",
         "Vue.js single-page application frontend with Vuex state management and Webpack bundling.",
         "Node.js REST API backend using MongoDB via Mongoose."
-      ]
+      ],
+      link: "https://powerrank.pc.codes"
     },
     {
       name: "Inspopaper",
       highlights: [
         "App available on Slack app directory that creates novelty desktop wallpapers on demand from messages posted in Slack.",
         "Serverless architecture using Python Google Cloud Functions and Google Cloud Blob Storage."
-      ]
+      ],
+      link: "https://inspopaper.fun"
     },
     // {
     //   name: "Live Audience Voting",
     //   highlights: [
     //     "Vue.js application using D3.js and Socket.io to collect and visualize audience votes via smartphone."
-    //   ]
+    //   ],
+    //   link: ""
     // },
     {
       name: "Powerpoint Generator",
       highlights: [
         "Procedurally genrates PowerPoint presentations for fun or in emergency situations."
-      ]
+      ],
+      link: "https://forgotmyslides.xyz"
     }
   ]
 };
