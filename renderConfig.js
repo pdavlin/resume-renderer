4fb4598bca73da8df9a627dfcc59fa8ec29d3d98
Price Comstock <price.comstock@gmail.com>
2020-02-03 09:35:04 -0500

    dockerized, not tested!

--- renderConfig.js
+++ renderConfig.js
@@ -1,24 +1,24 @@
 const resumeData = require("./data/resumeData");
 
-const prefix = "ComstockPrice-";
+const prefix = "price-comstock-";
 const renders = [
+  // {
+  //   title: "frontend-dev-1",
+  //   template: "2pretty.html",
+  //   data: resumeData.frontEndData
+  // },
+  // {
+  //   title: "backend-dev-1",
+  //   template: "2pretty.html",
+  //   data: resumeData.backEndData
+  // },
+  // {
+  //   title: "fullstack-dev-1",
+  //   template: "2pretty.html",
+  //   data: resumeData.fullStackData
+  // },
   {
-    title: "frontend-dev-1",
-    template: "2pretty.html",
-    data: resumeData.frontEndData
-  },
-  {
-    title: "backend-dev-1",
-    template: "2pretty.html",
-    data: resumeData.backEndData
-  },
-  {
-    title: "fullstack-dev-1",
-    template: "2pretty.html",
-    data: resumeData.fullStackData
-  },
-  {
-    title: "fullstack-dev-plain",
+    title: "fullstack-dev",
     template: "plain.html",
     data: resumeData.fullStackData
   },
@@ -31,12 +31,12 @@ const renders = [
     title: "backend-dev-plain",
     template: "plain.html",
     data: resumeData.backEndData
-  },
-  {
-    title: "ANONYMIZED",
-    template: "2pretty.html",
-    data: resumeData.anonymousData
   }
+  // {
+  //   title: "ANONYMIZED",
+  //   template: "2pretty.html",
+  //   data: resumeData.anonymousData
+  // }
 ];
 
 module.exports = {
